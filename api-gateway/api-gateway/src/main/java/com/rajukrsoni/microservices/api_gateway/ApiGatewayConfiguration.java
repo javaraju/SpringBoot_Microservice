4ace5036c2e14c3afd96c2e609ca535b2cfa79d1
rajukrsoni <info.krrajusoni@gmail.com>
2025-08-10 23:06:36 +0530

    Exploring Routes with Spring Cloud Gateway - V3

--- api-gateway/api-gateway/src/main/java/com/rajukrsoni/microservices/api_gateway/ApiGatewayConfiguration.java
+++ api-gateway/api-gateway/src/main/java/com/rajukrsoni/microservices/api_gateway/ApiGatewayConfiguration.java
@@ -1,28 +1,34 @@
 package com.rajukrsoni.microservices.api_gateway;
 
-import org.springframework.cloud.gateway.route.Route;
 import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.Buildable;
-import org.springframework.cloud.gateway.route.builder.PredicateSpec;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
-import java.util.function.Function;
 
 @Component
 public class ApiGatewayConfiguration {
 
     @Bean
     public RouteLocator gatewayRouter(RouteLocatorBuilder builder){
-        Function<PredicateSpec, Buildable<Route>> routeFunction
-                = p-> p.path("/get")
-                .filters(f-> f
-                        .addRequestHeader("MyHeader", "MyURI")
-                        .addRequestParameter("Param", "MyValue"))
-                .uri("http://httpbin.org:80");
-
-        return builder.routes().route(routeFunction).build();
+        return builder.routes()
+                .route(p-> p.path("/get")
+                        .filters(f-> f
+                                .addRequestHeader("MyHeader", "MyURI")
+                                .addRequestParameter("Param", "MyValue"))
+                        .uri("http:/httpbin.org:80"))
+                .route(p-> p.path("/currency-exchange/**")
+                        .uri("lb://currency-exchange"))
+                .route(p-> p.path("/currency-conversion/**")
+                        .uri("lb://currency-conversion"))
+                .route(p-> p.path("/currency-conversion-feign/**")
+                        .uri("lb://currency-conversion"))
+                //new url created by own and redirect to old url
+                .route(p-> p.path("/currency-conversion-new/**")
+                        .filters(f-> f.rewritePath("/currency-conversion-new/(?<segment>.*)",
+                        "/currency-conversion-feign/${segment}"))
+                        .uri("lb://currency-conversion"))
+                .build();
     }
 
 }
