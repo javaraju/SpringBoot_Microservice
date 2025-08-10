cd8ea47995dfde6b0df401768277446503e0a737
rajukrsoni <info.krrajusoni@gmail.com>
2025-08-10 22:07:11 +0530

    adde header and param value in it

--- api-gateway/api-gateway/src/main/java/com/rajukrsoni/microservices/api_gateway/ApiGatewayConfiguration.java
+++ api-gateway/api-gateway/src/main/java/com/rajukrsoni/microservices/api_gateway/ApiGatewayConfiguration.java
@@ -17,7 +17,12 @@ public class ApiGatewayConfiguration {
     public RouteLocator gatewayRouter(RouteLocatorBuilder builder){
         Function<PredicateSpec, Buildable<Route>> routeFunction
                 = p-> p.path("/get")
+                .filters(f-> f
+                        .addRequestHeader("MyHeader", "MyURI")
+                        .addRequestParameter("Param", "MyValue"))
                 .uri("http://httpbin.org:80");
+
         return builder.routes().route(routeFunction).build();
     }
+
 }
