20a6b3b9f2720a13915c9d1151e0ce8bd682ffc7
rajukrsoni <info.krrajusoni@gmail.com>
2025-08-25 12:48:32 +0530

    added distributed tracing (Zipkin for log, matrix, error) It helps you collect, visualize, and analyze trace data from requests flowing through multiple microservices.

--- currency-exchange-service/currency-exchange-service/src/main/java/com/rajukrsoni/microservices/currency_exchange_service/CurrencyExchangeController.java
+++ currency-exchange-service/currency-exchange-service/src/main/java/com/rajukrsoni/microservices/currency_exchange_service/CurrencyExchangeController.java
@@ -1,6 +1,8 @@
 package com.rajukrsoni.microservices.currency_exchange_service;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +15,8 @@ import java.math.BigDecimal;
 @RestController
 public class CurrencyExchangeController {
 
+    private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
+
     @Autowired
     private Environment environment;
     @Autowired
@@ -22,6 +26,14 @@ public class CurrencyExchangeController {
     public CurrencyExchange retrieveExchangeValue(
             @PathVariable String from,
             @PathVariable String to){
+
+        logger.info("retrieveExchangeValue called with {} to {}", from, to);
+//        [currency-exchange] [nio-8000-exec-1] [d007ab1b1f4dba8e7207a8eac7cad84e-a95b4efb5fae8362]
+//        c.r.m.c.CurrencyExchangeController       : retrieveExchangeValue called with USD to INR
+
+//        here logger gave above message in console with some id ([d007ab1b1f4dba8e7207a8eac7cad84e-a95b4efb5fae8362] )
+//        that is assing for every request by micrometer (dependency check in pom)
+
 
 //        CurrencyExchange currencyExchange = new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(50));
         CurrencyExchange currencyExchange = currencyExchangeRepository.findByFromAndTo(from, to);
